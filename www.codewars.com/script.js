18d4738f5cbb8b91dd4e501f6d3c5e2a9a62a19d
songjinzhong <648660165@qq.com>
2016-10-17 16:53:08 +0800

    new work

--- www.codewars.com/script.js
+++ www.codewars.com/script.js
@@ -450,7 +450,7 @@ log(solution("VII"))
 Undo/Redo
 https://www.codewars.com/kata/undo-slash-redo/train/javascript
 */
-
+/*
 function undoRedo(object) {
 	return {
 		dolist : [],
@@ -505,3 +505,149 @@ var test = undoRedo(obj);
 test.get("x")
 test.del("x")
 test.undo();
+*/
+/*
+ * base64
+ *  
+ */
+ /*
+//Extend the String object with toBase64() and fromBase64() functions
+// private method for UTF-8 encoding  
+var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function _utf8_encode(string) {  
+    string = string.replace(/\r\n/g,"\n");  
+    var utftext = "";  
+    for (var n = 0; n < string.length; n++) {  
+        var c = string.charCodeAt(n);  
+        if (c < 128) {  
+            utftext += String.fromCharCode(c);  
+        } else if((c > 127) && (c < 2048)) {  
+            utftext += String.fromCharCode((c >> 6) | 192);  
+            utftext += String.fromCharCode((c & 63) | 128);  
+        } else {  
+            utftext += String.fromCharCode((c >> 12) | 224);  
+            utftext += String.fromCharCode(((c >> 6) & 63) | 128);  
+            utftext += String.fromCharCode((c & 63) | 128);  
+        }  
+
+    }  
+    return utftext;  
+}  
+
+// private method for UTF-8 decoding  
+function _utf8_decode(utftext) {  
+    var string = "";  
+    var i = 0;  
+    var c = c1 = c2 = 0;  
+    while ( i < utftext.length ) {  
+        c = utftext.charCodeAt(i);  
+        if (c < 128) {  
+            string += String.fromCharCode(c);  
+            i++;  
+        } else if((c > 191) && (c < 224)) {  
+            c2 = utftext.charCodeAt(i+1);  
+            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));  
+            i += 2;  
+        } else {  
+            c2 = utftext.charCodeAt(i+1);  
+            c3 = utftext.charCodeAt(i+2);  
+            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));  
+            i += 3;  
+        }  
+    }  
+    return string;  
+}
+function encode(input){
+    var output = "";  
+    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;  
+    var i = 0;  
+    input = _utf8_encode(input);  
+    while (i < input.length) {  
+        chr1 = input.charCodeAt(i++);  
+        chr2 = input.charCodeAt(i++);  
+        chr3 = input.charCodeAt(i++);  
+        enc1 = chr1 >> 2;  
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);  
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);  
+        enc4 = chr3 & 63;  
+        if (isNaN(chr2)) {  
+            enc3 = enc4 = 64;  
+        } else if (isNaN(chr3)) {  
+            enc4 = 64;  
+        }  
+        output = output +  
+        _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +  
+        _keyStr.charAt(enc3) + _keyStr.charAt(enc4);  
+    }  
+    return output;
+}
+function decode(input){
+    var output = "";  
+    var chr1, chr2, chr3;  
+    var enc1, enc2, enc3, enc4;  
+    var i = 0;  
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");  
+    while (i < input.length) {  
+        enc1 = _keyStr.indexOf(input.charAt(i++));  
+        enc2 = _keyStr.indexOf(input.charAt(i++));  
+        enc3 = _keyStr.indexOf(input.charAt(i++));  
+        enc4 = _keyStr.indexOf(input.charAt(i++));  
+        chr1 = (enc1 << 2) | (enc2 >> 4);  
+        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);  
+        chr3 = ((enc3 & 3) << 6) | enc4;  
+        output = output + String.fromCharCode(chr1);  
+        if (enc3 != 64) {  
+            output = output + String.fromCharCode(chr2);  
+        }  
+        if (enc4 != 64) {  
+            output = output + String.fromCharCode(chr3);  
+        }  
+    }  
+    output = _utf8_decode(output);  
+    return output;  
+}
+String.prototype.toBase64 = function(){
+  return encode(this);
+}
+String.prototype.fromBase64 = function(){
+  return decode(this);
+}
+log("this is a string!!".toBase64());
+log('dGhpcyBpcyBhIHN0cmluZyEh'.fromBase64());
+*/
+//base answer
+String.prototype.toBase64 = function() {
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+  var encoded = '';
+  
+  for(var i=0; i < this.length; i+=3) {
+    var c1 = this.charCodeAt(i), 
+        c2 = this.charCodeAt(i+1), 
+        c3 = this.charCodeAt(i+2);
+    encoded += chars[(c1 & 0xFC) >> 2];
+    encoded += chars[((c1 & 0x03) << 4) | ((c2 & 0xF0) >> 4)];
+    encoded += chars[((c2 & 0x0F) << 2) | ((c3 & 0xC0) >> 6)];
+    encoded += chars[c3 & 0x3F];
+  }
+  
+  return encoded;
+};
+
+String.prototype.fromBase64 = function() {
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+  var decoded = '';
+  
+  for(var i=0; i < this.length; i+=4) {
+    var c1 = chars.indexOf(this[i]), 
+        c2 = chars.indexOf(this[i+1]), 
+        c3 = chars.indexOf(this[i+2]),
+        c4 = chars.indexOf(this[i+3]);
+    decoded += String.fromCharCode(((c1) << 2) | ((c2 & 0x30) >> 4));
+    decoded += String.fromCharCode(((c2 & 0x0F) << 4) | ((c3 & 0xFC) >> 2));
+    decoded += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+  }
+  
+  return decoded;
+};
+log("this is a string!!".toBase64());
+log('dGhpcyBpcyBhIHN0cmluZyEh'.fromBase64());
